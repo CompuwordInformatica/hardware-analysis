05062e1a81dcf77a95014c603ec9cc43292124cb
Compuword Soluções <148906231+CompuwordInformatica@users.noreply.github.com>
2024-03-18 08:58:45 -0300

    Update InstallApps.cmd

--- InstallApps.cmd
+++ InstallApps.cmd
@@ -2,7 +2,6 @@ choco install winrar -y --ignore-checksums
 choco install googlechrome -y --ignore-checksums
 choco install adobereader -y --ignore-checksums
 choco install jre8 -y --ignore-checksums
-choco install python3 -y --ignore-checksums
-choco install git -y --ignore-checksums
+choco install anydesk -y --ignore-checksums
 
 
